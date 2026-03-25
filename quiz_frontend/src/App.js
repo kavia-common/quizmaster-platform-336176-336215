2310def352a5f3446396a5d2452bf4b387e84618
Kavia AI <production@kavia.ai>
2026-03-25 12:50:33 +0000

    Checkpoint 1 - cmfeeabbb3

---
+++ quiz_frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Home from "./pages/Home";
+
+// PUBLIC_INTERFACE
+function App() {
+  /** Root application component. Renders the Home page. */
+  return <Home />;
+}
+
+export default App;
